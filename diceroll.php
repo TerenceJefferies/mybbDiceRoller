eba93a5173e5e830808bc9af7d5db7f0a93b343c
Terence Jefferies <terencejefferies@hotmail.com>
2015-03-02 19:55:19 +0000

    Updated with finalised settings, feature now operational

--- diceroll.php
+++ diceroll.php
@@ -1,7 +1,13 @@
 <?php
 
+    /*MyBB Dice Roller - Permenant Rolls, rolls created using this plugin cannot be edited.*/
+
     if(!defined("IN_MYBB")) { die("Unauthorized"); }
-    
+
+    $diceroll_salt = "";
+    $diceroll_verificationvisible = true;
+    $diceroll_enable = false;
+
     function diceroll_info() {
         return array(
             "name"                  =>                  "TJs Dice Roll Plugin",
@@ -14,7 +20,7 @@
             "compatibility"         =>                  "18*"
         );
     }
-    
+
     function diceroll_activate() {
         global $db;
         $diceroll_group = array(
@@ -25,10 +31,10 @@
             "disporder"             =>                  "1",
             "isdefault"             =>                  "0"
         );
-        
+
         $db -> insert_query("settinggroups",$diceroll_group);
         $gid = $db -> insert_id();
-        
+
         $diceroll_setting = array(
             'sid'                   =>                  'NULL',
             "name"                  =>                  "diceroll_enabled",
@@ -39,32 +45,77 @@
             "disporder"             =>                  1,
             "gid"                   =>                  intval($gid)
         );
-        
+
         $db -> insert_query("settings",$diceroll_setting);
+
+        $diceroll_setting = array(
+            'sid'                   =>                  'NULL',
+            "name"                  =>                  "diceroll_salt",
+            "title"                 =>                  "Dice role salt (for verification)",
+            "description"           =>                  "Set this value to be a string only your admins know. They can use it to verify the authenticity of roles.",
+            "optionscode"           =>                  "text",
+            "value"                 =>                  "OJISndiujn91udndu9nasdu",
+            "disporder"             =>                  3,
+            "gid"                   =>                  intval($gid)
+        );
+
+        $db -> insert_query("settings",$diceroll_setting);
+
+        $diceroll_setting = array(
+            'sid'                   =>                  'NULL',
+            "name"                  =>                  "diceroll_verificationvisible",
+            "title"                 =>                  "Is the verification code visible?",
+            "description"           =>                  "If set to YES, the verification code will be visible on posts",
+            "optionscode"           =>                  "yesno",
+            "value"                 =>                  "1",
+            "disporder"             =>                  2,
+            "gid"                   =>                  intval($gid)
+        );
+
+        $db -> insert_query("settings",$diceroll_setting);
+
         rebuild_settings();
-        
-        $db -> add_column("posts","diceroll","text");  
+
+        $db -> add_column("posts","diceroll","text");
     }
-    
-    function diceroll_uninstall() {
+
+    function diceroll_deactivate() {
         global $db;
-        $db -> query("DELETE FROM " . TABLE_PREFIX . "settings WHERE name IN ('diceroll_enable')");
+        $db -> query("DELETE FROM " . TABLE_PREFIX . "settings WHERE name IN ('diceroll_enabled')");
+        $db -> query("DELETE FROM " . TABLE_PREFIX . "settings WHERE name IN ('diceroll_salt')");
+        $db -> query("DELETE FROM " . TABLE_PREFIX . "settings WHERE name IN ('diceroll_verificationvisible')");
         $db -> query("DELETE FROM " . TABLE_PREFIX . "settinggroups WHERE name = 'TJsDiceRollPlugin'");
-        
+
         $db -> drop_column("posts","diceroll");
-        
+
         rebuild_settings();
     }
-    
+
+    $plugins -> add_hook("global_start","diceroll_init");
+    function diceroll_init() {
+        global $db;
+        global $diceroll_salt;
+        global $diceroll_verificationvisible;
+        global $diceroll_enable;
+        global $plugins;
+        $result = $db -> query("SELECT * FROM " . TABLE_PREFIX . "settings WHERE name IN ('diceroll_salt')");
+        $diceroll_salt = $db -> fetch_field($result,"value");
+        $result = $db -> query("SELECT * FROM " . TABLE_PREFIX . "settings WHERE name IN ('diceroll_verificationvisible')");
+        $diceroll_verificationvisible = $db -> fetch_field($result,"value");
+        $result = $db -> query("SELECT * FROM " . TABLE_PREFIX . "settings WHERE name IN ('diceroll_enabled')");
+        $diceroll_enable = $db -> fetch_field($result,"value");
+        if($diceroll_enable) {
+          $plugins->add_hook("datahandler_post_insert_post", "diceroll_roll");
+          $plugins->add_hook("datahandler_post_insert_thread_post", "diceroll_roll");
+          $plugins -> add_hook("postbit","diceroll_appendrolls");
+        }
+        //var_dump($diceroll_enable);
+    }
+
     /**************************************************************************/
-    
-    $plugins->add_hook("datahandler_post_insert_post", "diceroll_roll");
-    $plugins->add_hook("datahandler_post_insert_thread_post", "diceroll_roll");
-    //$plugins->add_hook("datahandler_post_update", "dymy_dice_do");
-    
-    $plugins -> add_hook("postbit","diceroll_appendrolls");
-    
+
     function diceroll_roll(&$post) {
+        global $db;
         $msg = $post->post_insert_data['message'];
         $rollArray = array();
         $matches = array();
@@ -73,36 +124,52 @@
             $rollName = $match[1];
             $numberOfRolls = $match[2];
             $sidedDice = $match[3];
-            if($rollName && $numberOfRolls > 0 && $numberOfRolls < 999 && $sidedDice > 0 && $sidedDice < 999) {
+            if($rollName && $numberOfRolls > 0 && $numberOfRolls < 16 && $sidedDice > 0 && $sidedDice < 36) {
                 for($roll = 0; $roll < $numberOfRolls; $roll ++) {
                     $rollArray[$rollName][$sidedDice][$roll] = rand(1,$sidedDice);
                 }
             }
-       } 
+            $post -> post_insert_data['message'] = str_replace($match[0],"",$post -> post_insert_data['message']);
+       }
        $saveString = serialize($rollArray);
-       $post->post_insert_data['diceroll'] = $saveString;
+       $post->post_insert_data['diceroll'] = $db -> escape_string($saveString);
     }
-    
+
     function diceroll_appendrolls(&$post) {
+        global $diceroll_salt;
+        global $diceroll_verificationvisible;
         if($post['diceroll']) {
             $rollArray = @unserialize($post['diceroll']);
             if(is_array($rollArray)) {
-                $post['message'] .= '<br /><br />Dice rolls attached to this thread:<br />';
+                $post['message'] .= '<br /><br /><span style="color:purple;"><i>Dice rolls attached to this post:</i><br />';
+                if($diceroll_verificationvisible) {
+                  $post['message'] .= 'Verification Code: ' . sha1($diceroll_salt . $post['pid']) . '<br />';
+                }
                 foreach($rollArray as $rollName => $sidedDice) {
-                    $count = 0;
-                    $post['message'] .= '<br /><i>' . $rollName . ' (' . $numberOfSides . ' sided dice):';
                     foreach($sidedDice as $numberOfSides => $result) {
+                        $post['message'] .= '<br /><i>' . $rollName . ' (' . $numberOfSides . ' sided dice):';
+                        $appendRolls = '';
+                        $totalResult = 0;
+                        $count = 0;
                         foreach($result as $rollOffset => $rollResult) {
-                            if($count > 0) { $post['message'] .= ','; }
-                            $post['message'] .= $rollOffset . '=' . $rollResult;
+                            if($count > 0) { $appendRolls .= ','; }
+                            $appendRolls .= ($rollOffset + 1) . '=' . $rollResult;
+                            $totalResult += $rollResult;
                             $count ++;
                         }
+                        if($count > 1) {
+                          $appendRolls .= ' = ' . $totalResult;
+                        } else {
+                          $appendRolls = $totalResult;
+                        }
+                        $post['message'] .= $appendRolls;
                     }
                     $post['message'] .= '</i>';
                 }
+                $post['message'] .= '</span>';
             }
         }
     }
-    
-    
+
+
 ?>
